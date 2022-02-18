ea20119601b64532b7d02149171ad56375cd96f8
SimoneBarbagallo <98325910+SimoneBarbagallo@users.noreply.github.com>
2022-02-18 10:20:01 +0100

    Es3

---
+++ BarbagalloEs3/BarbagalloEs3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BarbagalloEs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contoparola = 0;
+            string parola= "";
+            while (parola !="fine")
+            {
+                Console.WriteLine("inserisci una parola");
+                parola = Console.ReadLine();
+                
+                char primalettera = parola [0];
+                int lungh = parola.Length;
+                char ultimalettera = parola [lungh - 1];
+                if (primalettera == ultimalettera)
+                {
+                    contoparola++;
+                }      
+            }
+            Console.WriteLine($"il numero delle parole che iniziano e finiscono con la stessa lettera è di {contoparola}");
+        }
+    }
+}
